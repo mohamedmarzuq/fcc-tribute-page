4880908040a6b71be037dce0e8b576f572c84cf8
mohamedmarzuq <mohamedmarzuq2005@gmail.com>
2021-10-16 20:03:17 +0530

    swiper.js initialised

--- script.js
+++ script.js
@@ -1,21 +1,15 @@
-const swiper = new Swiper('.swiper', {
-    // Optional parameters
-    direction: 'vertical',
-    loop: true,
-  
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-    },
-  
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  
-    // And if we need scrollbar
-    scrollbar: {
-      el: '.swiper-scrollbar',
-    },
-  });  
+var swiper = new Swiper(".mySwiper", {
+  spaceBetween: 30,
+  //effect: "fade",
+  //loop:true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  }//, autoplay: {
+    //delay: "3000"
+  //}
+});
